2b5982aece25d22a03b6fc8611ddc01b047c5ba6
unknown <Administrator@SKCC22N01071.SKCC.NET>
2025-05-12 11:18:45 +0900

    Initial commit with Spring Boot project setup

---
+++ src/main/java/com/contractrisk/contract_risk_detector/ContractRiskDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.contractrisk.contract_risk_detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractRiskDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractRiskDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contractrisk/contract_risk_detector/ContractRiskDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contractrisk.contract_risk_detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractRiskDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
